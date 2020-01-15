b65e97db0d0cd692d4595ae9f4084367fdd84833
Robert Tisma <942951+rtisma@users.noreply.github.com>
2020-01-15 13:47:29 -0500

    cleaned up

--- src/main/java/com/roberttisma/tools/intermediate_song_importer/model/ProfileConfig.java
+++ src/main/java/com/roberttisma/tools/intermediate_song_importer/model/ProfileConfig.java
@@ -1,15 +1,13 @@
 package com.roberttisma.tools.intermediate_song_importer.model;
 
-import static com.roberttisma.tools.intermediate_song_importer.util.Fields.mergeField;
-import static com.roberttisma.tools.intermediate_song_importer.util.Fields.mergeMergableField;
-
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+
+import static com.roberttisma.tools.intermediate_song_importer.util.Fields.mergeField;
+import static com.roberttisma.tools.intermediate_song_importer.util.Fields.mergeMergableField;
 
 @Data
 @Builder
@@ -24,15 +22,9 @@ public class ProfileConfig implements Mergable<ProfileConfig> {
 
   @Override
   public void merge(@NonNull ProfileConfig mergeIn) {
-    mergeProfileField(ProfileConfig::getName, ProfileConfig::setName, mergeIn);
+    mergeField(ProfileConfig::getName, ProfileConfig::setName, this, mergeIn);
     mergeMergableField(ProfileConfig::getTargetSong, this, mergeIn);
     mergeMergableField(ProfileConfig::getSourceSong, this, mergeIn);
   }
 
-  private void mergeProfileField(
-      Function<ProfileConfig, String> getter,
-      BiConsumer<ProfileConfig, String> setter,
-      ProfileConfig mergeIn) {
-    mergeField(getter, setter, this, mergeIn);
-  }
 }
