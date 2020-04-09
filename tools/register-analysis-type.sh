3ad3ec3f67287b28d2b3c2c54dc90322eba9afd0
Robert Tisma <robtisma@example.com>
2020-04-09 13:36:52 +0000

    updated

--- tools/register-analysis-type.sh
+++ tools/register-analysis-type.sh
@@ -7,7 +7,8 @@ if [ ! -f $register_analysis_type_file ]; then
 	exit 2
 fi
 
-curl -XPOST \ 
-	--header 'Authorization: Bearer f69b726d-d40f-4261-b105-1ec7e6bf04d5' \
-	-d @$register_analysis_type_file \
-	'http://localhost:9080/schemas'
+curl -XPOST \
+    -H 'Content-Type: application/json'  \
+	-H 'Authorization: Bearer f69b726d-d40f-4261-b105-1ec7e6bf04d5' \
+	-d "@sequencing_experiment.json" \
+	http://localhost:9080/schemas
